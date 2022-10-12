97eeb664dddd17d7247a26bd4c84193337f0cb9d
Sathursan <balansathu1214@gmail.com>
2022-10-12 18:41:59 +0530

    Manager Site

---
+++ client/src/pages/Manager.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import PaymentList from '../components/manager/PaymentList'
+import MNavbar from '../components/nav/Mnavbar'
+
+
+const Manager = () => {
+    return (
+        <div>
+            <MNavbar />
+            {/* <PaymentList/> */}
+        </div>
+    )
+}
+
+export default Manager
